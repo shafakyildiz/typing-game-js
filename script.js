f917c25fd0bbdd652740c5507626175b6fadba04
Şafak Yıldız <shafakyildiz@gmail.com>
2021-03-15 15:09:47 +0300

    Edit script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-script.js
+const word = document.getElementById('word');
+const text = document.getElementById('text');
+const scoreEl = document.getElementById('score');
+const timeEl = document.getElementById('time');
+const endgameEl = document.getElementById('end-game');
+const settingsBtn = document.getElementById('settings-btn');
+const settings = document.getElementById('settings');
+const settingsForm = document.getElementById('settings-form');
+const difficultySelect = document.getElementById('difficulty');
